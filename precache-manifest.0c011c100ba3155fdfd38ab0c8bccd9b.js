f87a51204a9a31dad7608dbcf381305fd6e1a076
Oleksandr Rulenko <rulenko4@gmail.com>
2020-03-30 15:24:28 +0200

    builded social-network

---
+++ precache-manifest.0c011c100ba3155fdfd38ab0c8bccd9b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abdfb668af2da0c5463b24b6c8e05e11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e925edc27c364471362a",
+    "url": "/static/css/4.4ff5770c.chunk.css"
+  },
+  {
+    "revision": "327fffa2e8e74d8e2cb6",
+    "url": "/static/css/5.5524e1eb.chunk.css"
+  },
+  {
+    "revision": "c3bf6356e40b7abe17df",
+    "url": "/static/css/main.53d322d5.chunk.css"
+  },
+  {
+    "revision": "f07a5c687a2e82339fb2",
+    "url": "/static/js/2.52ae4116.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.52ae4116.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd977556df49efb3dd75",
+    "url": "/static/js/3.10cf1b97.chunk.js"
+  },
+  {
+    "revision": "b9818e79487d59f56d62a09d3ccf37c4",
+    "url": "/static/js/3.10cf1b97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e925edc27c364471362a",
+    "url": "/static/js/4.cce5ef33.chunk.js"
+  },
+  {
+    "revision": "327fffa2e8e74d8e2cb6",
+    "url": "/static/js/5.2f692875.chunk.js"
+  },
+  {
+    "revision": "c3bf6356e40b7abe17df",
+    "url": "/static/js/main.cbb845a2.chunk.js"
+  },
+  {
+    "revision": "7248f4cbe652aa62f3fb",
+    "url": "/static/js/runtime-main.4e4328bf.js"
+  },
+  {
+    "revision": "98de2c3b80f2896707cce174216d8208",
+    "url": "/static/media/preloader.98de2c3b.gif"
+  },
+  {
+    "revision": "9f7ae07678b2f6a8571452aa3ae40629",
+    "url": "/static/media/user.9f7ae076.png"
+  }
+]);
